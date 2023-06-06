da395cb4ef2c18d08e71b4b0c9dce28ed9ca0331
Carolina Figueroa <andreacarolina.f28@gmail.com>
2023-06-05 23:38:45 -0600

    base

---
+++ srcs/alimento.h
@@ -0,0 +1,41 @@
+// --- CLASE ALIMENTO ---
+// Jorge Luis Nájera Espinosa - A01424106
+// Andrea Carolina Figueroa Orihuela - A01424250
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#pragma once
+
+
+class Alimento {
+private:
+    string nombre;   // Nombre del alimento
+    int calorias;         // Cantidad de calorías en el alimento
+
+public:
+    //CONSTRUCTORES
+    //Constructor vacio
+    Alimento() {} 
+    //Constructor default
+    Alimento() = default;
+    // Constructor que inicializa todos los atributos
+    Alimento(const string& nombre, int calorias)  
+            : nombre(nombre),  calorias(calorias) {}
+    //Constructor copia
+    Alimento(Alimento &&otro) 
+            : nombre(otro.nombre), calorias(otro.calorias) {}
+    //Destructor
+    ~Alimento(){}
+
+    //Setters de atributos
+    void setNombre(const string &nombre)    { this -> nombre = nombre; }
+    void setNombre(const int &calorias)     { this -> calorias = calorias; }
+    // Getters de atributos
+    const string& getNombre() const         { return nombre; }
+    int getCalorias() const                 { return calorias; }
+
+};
+
---
+++ srcs/grupoAlimento.h
@@ -0,0 +1,43 @@
+// --- CLASE ALIMENTO ---
+// Jorge Luis Nájera Espinosa - A01424106
+// Andrea Carolina Figueroa Orihuela - A01424250
+
+#include <iostream>
+#include <string>
+#include <string>
+#include <vector>
+#include "alimento.h"
+
+using namespace std;
+
+#pragma once
+
+
+class Grupo {
+private:
+    string nombre;               // Nombre del grupo
+    vector<const Alimento*> alimentos;  // Alimentos que forman este alimento
+
+public:
+    // CONSTRUCTORES
+    //Constructor vacio
+    Grupo () {}
+    //Constructor que inicializa con dato nombre
+    Grupo (const string &nombre)
+            : nombre(nombre){}
+    //Constructor que inicializa todos los datos
+    Grupo (const string &nombre, const vector<const Alimento*> &alimentos)
+            : nombre(nombre), alimentos(alimentos) {}
+    //Constructor copia
+    Grupo (const Grupo &otro) 
+            : nombre(otro.nombre), alimentos(otro.alimentos) {}
+    //Destructor
+    ~Grupo(){}
+    
+    //Setters
+    void setNombre(const string &nombre) { this -> nombre = nombre; }
+    void agregarAlimento(const Alimento* &alimento) { alimentos.push_back(alimento); }
+    //Getters
+    const string& getNombre() const     { return nombre; }
+    const vector<const Alimento*>& getAlimentos() const     { return alimentos; }
+};
